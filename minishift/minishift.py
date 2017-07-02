876f73678438cc9af5b344be5923208127e85f85
Ian Miell <ian.miell@gmail.com>
2017-07-02 11:10:15 +0100

    initial

---
+++ minishift/minishift.py
@@ -0,0 +1,14 @@
+import shutit
+
+s1 = shutit.create_session('bash')
+while True:
+	status = s1.send_and_get_output('minishift status')
+	if status == 'Running':
+		s1.send('eval $(minishift oc-env)')
+		break
+	else:
+		print status
+		s1.send('minishift start')
+		s1.send('eval $(minishift oc-env)')
+	s1.send('oc login -u system:admin')
+s1.interact()
