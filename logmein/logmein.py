6c311ebaa404e1b7fb3d1d5f7d244875be4b1b80
Ian Miell <ian.miell@gmail.com>
2017-07-10 13:15:08 +0100

    logmein

--- logmein/logmein.py
+++ logmein/logmein.py
@@ -1,5 +1,18 @@
 import json
+import shutit
 
-
+password_dict = None
 # password json file - must be 0400 - shutit issecure function
-servers = json.loads(open('a.json').read())
+stat file
+if file exists and has bad perms,
+	exit
+if file exists read in file
+
+
+server_dict = json.loads(open('servers.json').read())
+
+server_list = servers.keys()
+print server_list
+for item in server_dict:
+	print item
+	print servers[item]
