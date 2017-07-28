52ef373efc3c313df387617d6bcd21b28adcc9e2
Ian Miell <ian.miell@gmail.com>
2017-07-28 09:42:02 +0100

    console

--- minishift/minishift.py
+++ minishift/minishift.py
@@ -1,15 +1,16 @@
 import shutit
 
-s1 = shutit.create_session('bash')
+# Tested on OSx only
+s1 = shutit.create_session('bash',loglevel='debug')
+#s1 = shutit.create_session('bash')
 while True:
 	status = s1.send_and_get_output('minishift status')
 	if status == 'Running':
 		s1.send('eval $(minishift oc-env)')
 		break
 	else:
-		print status
 		s1.send('minishift start')
 		s1.send('eval $(minishift oc-env)')
 s1.send('oc login -u system:admin')
-s1.login('oc exec -ti mysql-1-2gc84 bash')
+s1.send('minishift console')
 s1.interact()
