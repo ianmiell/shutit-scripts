940b65db1527edb769cb3c24b2f5eedfa04e58dc
Ian Miell <ian.miell@gmail.com>
2017-07-28 20:52:05 +0100

    main

--- minishift/minishift.py
+++ minishift/minishift.py
@@ -1,19 +1,23 @@
 import shutit
 
-# Tested on OSx only
-s1 = shutit.create_session('bash',loglevel='debug')
-# TODO: get minishift
-s1.send('minishift update',{'want to update':'y'})
-while True:
-	status = s1.send_and_get_output('minishift status')
-	if status == 'Running':
-		s1.send('eval $(minishift oc-env)')
-		break
-	else:
-		s1.send('minishift start')
-		s1.send('eval $(minishift oc-env)')
-		s1.send('minishift start --vm-driver virtualbox')
-		s1.send('eval $(minishift oc-env)')
-s1.send('oc login -u system:admin')
-s1.send('minishift console')
-s1.interact()
+# IF YOU SEE GENERAL FAILURES, TRY:
+#sudo rm -rf ~/.minishift/
+
+def startup():
+	s1 = shutit.create_session('bash',loglevel='debug')
+	# TODO: get minishift itself?
+	s1.send('minishift update',{'want to update':'y'})
+	while True:
+		status = s1.send_and_get_output('minishift status')
+		if status == 'Running':
+			s1.send('eval $(minishift oc-env)')
+			break
+		else:
+			s1.send('minishift start --vm-driver virtualbox')
+			s1.send('eval $(minishift oc-env)')
+	s1.send('oc login -u system:admin')
+
+if __name__ == "__main__":
+	startup()
+	s1.send('minishift console')
+	s1.interact()
