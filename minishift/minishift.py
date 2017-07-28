4e633a2bea789b0bddddb88ecab5222071a68fe7
Ian Miell <ian.miell@gmail.com>
2017-07-28 10:48:48 +0100

    minishift

--- minishift/minishift.py
+++ minishift/minishift.py
@@ -2,7 +2,8 @@ import shutit
 
 # Tested on OSx only
 s1 = shutit.create_session('bash',loglevel='debug')
-#s1 = shutit.create_session('bash')
+# TODO: get minishift
+s1.send('minishift update',{'want to update':'y'})
 while True:
 	status = s1.send_and_get_output('minishift status')
 	if status == 'Running':
@@ -10,6 +11,8 @@ while True:
 		break
 	else:
 		s1.send('minishift start')
+		s1.send('eval $(minishift oc-env)')
+		s1.send('minishift start --vm-driver virtualbox')
 		s1.send('eval $(minishift oc-env)')
 s1.send('oc login -u system:admin')
 s1.send('minishift console')
