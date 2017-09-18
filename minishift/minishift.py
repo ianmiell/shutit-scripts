bb82ff5d132c9ed72070cc0718ad1573ad867d3d
Ian Miell <ian.miell@gmail.com>
2017-09-18 19:00:42 +0100

    login first

--- minishift/minishift.py
+++ minishift/minishift.py
@@ -26,8 +26,8 @@ def startup():
 				s1.send('minishift addons enable cluster-admin')
 				s1.send('minishift start --vm-driver virtualbox')
 			s1.send('eval $(minishift oc-env)')
-	s1.send('oc adm policy add-cluster-role-to-user cluster-admin admin --as=system:admin')
 	login_as_root(s1)
+	s1.send('oc adm policy add-cluster-role-to-user cluster-admin admin --as=system:admin')
 	return s1
 
 
