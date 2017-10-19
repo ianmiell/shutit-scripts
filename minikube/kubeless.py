2dbf50d74d3dd2d5bfe7af7436e2e7a38529f9b1
Ian Miell <ian.miell@gmail.com>
2017-10-19 20:19:14 +0100

    gitignore

--- minikube/kubeless.py
+++ minikube/kubeless.py
@@ -17,4 +17,19 @@ if s1.send_and_get_output('kubectl get ns | grep -w kubeless | wc -l') == '0':
 	# No RBAC
 	s1.send('curl -sL https://github.com/kubeless/kubeless/releases/download/v0.2.3/kubeless-v0.2.3.yaml | kubectl create -f -')
 
+
+s1.send('rm -rf tmpkubeless && mkdir tmpkubeless')
+s1.send('cd tmpkubeless')
+s1.send('git clone https://github.com/kubeless/kubeless.git')
+s1.send('cd kubeless/examples')
+if s1.send_and_get_output('kubeless function list | grep -w get-python | wc -l') != '0':
+	s1.send('kubeless function delete get-python')
+s1.send('make get-python')
+s1.send('kubeless function call get-python')
+s1.pause_point('')
+s1.send('cd ../..')
+s1.send('rm -rf tmpkubeless')
+
+
+
 s1.pause_point('kubectl')
